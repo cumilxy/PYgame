ad941c401534ec9091cb2f1cc4672de079dac942
cumilxy <146631747+cumilxy@users.noreply.github.com>
2023-12-24 13:27:02 +0200

    Update gra.py

--- gra.py
+++ gra.py
@@ -36,7 +36,7 @@ class Object(sprite.Sprite):
     
     def move(self):
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 0: #vlivo2
+        if keys[K_LEFT] and self.rect.x > 100: #vlivo2
             self.rect.x -= self.speed
             for w in walls:
                 if sprite.collide_rect(chuvak2,w):
@@ -59,7 +59,7 @@ class Object(sprite.Sprite):
 
     direction = 'right'
     def move2(self):
-        if self.rect.x > 390:
+        if self.rect.x > 300:
             self.direction = 'left'
         if self.rect.x < 100:
             self.direction = 'right'
@@ -81,14 +81,27 @@ class Object(sprite.Sprite):
         else:
             self.rect.y += 4
 
+    def fire(self):
+        bullet = Bullet('bullet-removebg-preview.png',self.rect.centerx,self.rect.centery,15,15,10)
+        bullets.add(bullet)
+
+class Bullet(Object):
+    def update(self):
+        self.rect.y += 10
+        if self.rect.y > 1000:
+            self.kill()
+bullets = sprite.Group()
+
 #створення вікна
 window = display.set_mode((800,600))
 display.set_caption('Лабіринт')
 picture = transform.scale(image.load("bnacckck.png"),(800,600))
 
-chuvak1 = Object("chuvak1.png",90,200,60,50,4)
-chuvak2 = Object("chuvak2.jpg",30,100,60,50,8)
-chuvak3 = Object("b.png",500,100,60,50,4)
+chuvak1 = Object("chuvak1-removebg-preview.png",90,200,60,50,4)
+chuvak2 = Object("chuvak2-removebg-preview.png",30,100,60,50,8)
+chuvak3 = Object("b-removebg-preview.png",500,100,60,50,4)
+coin = Object("coin-removebg-preview.png",650,400,60,50,4)
+chuvak4 = Object("epf-removebg-preview.png",650,350,60,50,4)
 clock  = time.Clock()
 
 #створення стін
@@ -104,6 +117,8 @@ wall9 = Wall(61,94,158,400,190,220,10)
 wall10 = Wall(61,94,158,500,100,250,10)
 wall11 = Wall(61,94,158,610,200,10,250)
 wall12 = Wall(61,94,158,740,110,10,340)
+wall13 = Wall(61,94,158,0,350,200,10)
+
 
 walls = []
 walls.append(wall1)
@@ -120,36 +135,105 @@ walls.append(wall11)
 walls.append(wall12)
 #створення головного циклу
 game = True
+level1 = True
+level2 = False
 while game:
-    for e in event.get():
-        if e.type == QUIT:
-            game = False
-    
-    window.blit(picture,(0,0))
-    chuvak1.reset()
-    chuvak3.reset()
-    chuvak2.reset()
-    chuvak2.move()
-    chuvak1.move2()
-    chuvak3.move3()
-
-    wall1.draw_wall()
-    wall2.draw_wall()
-    wall3.draw_wall()
-    wall4.draw_wall()
-    wall5.draw_wall()
-    wall6.draw_wall()
-    wall7.draw_wall()
-    wall8.draw_wall()
-    wall9.draw_wall()
-    wall10.draw_wall()
-    wall11.draw_wall()
-    wall12.draw_wall()
-    if sprite.collide_rect(chuvak2,chuvak1) or sprite.collide_circle(chuvak2,chuvak3):
-        game = False
+    if level1:
+        for e in event.get():
+            if e.type == QUIT:
+                game = False
+            elif e.type == KEYDOWN:
+                if e.key == K_e:
+                    chuvak2.fire()
         
-    
-  
+        
+        
+        window.blit(picture,(0,0))
 
+        bullets.update()
+        bullets.draw(window)
+
+        chuvak1.reset()
+        chuvak3.reset()
+        chuvak2.reset()
+        chuvak4.reset()
+        coin.reset()
+        chuvak2.move()
+        chuvak1.move2()
+        chuvak3.move3()
+
+        wall1.draw_wall()
+        wall2.draw_wall()
+        wall3.draw_wall()
+        wall4.draw_wall()
+        wall5.draw_wall()
+        wall6.draw_wall()
+        wall7.draw_wall()
+        wall8.draw_wall()
+        wall9.draw_wall()
+        wall10.draw_wall()
+        wall11.draw_wall()
+        wall12.draw_wall()
+        wall13.draw_wall()
+       
+        if sprite.collide_rect(chuvak2,chuvak1) or sprite.collide_circle(chuvak2,chuvak3) or sprite.collide_rect(chuvak2,chuvak4):
+            game = False
+
+        if sprite.spritecollide(chuvak4,bullets,True):
+            chuvak4.rect.x = 2000
+        if sprite.collide_rect(chuvak2,coin):
+            level1 = False
+            level2 = True
+            chuvak1 = Object("chuvak1.png",90,200,60,50,4)
+            chuvak2 = Object("chuvak2.jpg",30,100,60,50,8)
+            chuvak3 = Object("b.png",500,100,60,50,4)
+            coin = Object("b.png",500,400,60,50,4)
+            
+            clock  = time.Clock()
+
+
+
+
+            #створення стін
+            wall1 = Wall(61,94,158,300,0,50,450)
+            wall2 = Wall(61,94,158,300,450,200,50)
+            wall3 = Wall(61,94,158,600,400,50,300)
+            wall4 = Wall(61,94,158,330,0,70,450)
+            
+
+            walls = []
+            walls.append(wall1)
+            walls.append(wall2)
+            walls.append(wall3)
+            
+            
+    
+    if level2:
+        for e in event.get():
+            if e.type == QUIT:
+                game = False
+        
+        window.blit(picture,(0,0))
+        chuvak1.reset()
+        chuvak3.reset()
+        chuvak2.reset()
+        
+        chuvak2.move()
+        chuvak1.move2()
+        chuvak3.move3()
+
+        wall1.draw_wall()
+        wall2.draw_wall()
+        wall3.draw_wall()
+        wall4.draw_wall()
+        
+       
+        if sprite.collide_rect(chuvak2,chuvak1) or sprite.collide_circle(chuvak2,chuvak3):
+            game = False
+
+        
+
+
+    
     display.update()
     clock.tick(60)
